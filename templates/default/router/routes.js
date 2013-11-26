36395599142061260689261b793534b2a57ba931
Adam Brodzinski <adambrodzinski@gmail.com>
2013-11-25 16:02:54 -0800

    add router
    adds implemetation to copy and move router.js
    to both/router.js.

---
+++ templates/default/router/routes.js
@@ -0,0 +1,12 @@
+//  Iron Router - for useage see https://github.com/EventedMind/iron-router
+
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Router.map(function () {
+
+  this.route('homeTemp', { path: '/'});
+
+}); // _end_router_
+
