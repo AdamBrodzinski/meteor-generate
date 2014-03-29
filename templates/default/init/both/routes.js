44d8c2a7e287f2140296e0c6b91436cdd67cc999
Adam Brodzinski <adambrodzinski@gmail.com>
2014-03-29 16:15:59 -0700

    add starter html and route

---
+++ templates/default/init/both/routes.js
@@ -0,0 +1,13 @@
+//  Iron Router - for useage see https://github.com/EventedMind/iron-router
+
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Router.map(function () {
+
+  // delete me and replace with your homepage
+  this.route('homeTemp', {path: '/'})
+
+});//<end-routes>
+
