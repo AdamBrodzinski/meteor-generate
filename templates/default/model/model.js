5df91b4507cde70c15fdb7412525b517eb16e6ab
Adam Brodzinski <adambrodzinski@gmail.com>
2015-03-22 20:18:52 -0400

    reworks model to be more secure
    now user auth is defaulted. cleans up syntax a bit

--- templates/default/model/model.js
+++ templates/default/model/model.js
@@ -1,3 +1,5 @@
+/*global __name-singular-pascal__:true, User */
+
 // CRUD facade to call Meteor methods more elegantly
 __name-singular-pascal__ = {
   create: function(data, callback) {
@@ -14,56 +16,105 @@ __name-singular-pascal__ = {
 };
 
 
-// ** NOTE**
-// all insert, update, and delete MiniMongo methods are disabled on the client.
-// this ensures more granular security & moves the security logic into the
-// meteor method. all mutation has to happen with the Meteor methods. these
-// methods are placed into the 'both' folder so that Meteor uses the methods as
-// stubs on the client, retaining the latency compensation. if you need to
-// obscure the model, move the method into the server directory. doing so will lose
-// latency compensation, a stub can be created on the client folder to re-enable
+// ** Security README **
 //
-// console.logs are added to help prevent forgetting to secure before shipping
+// all insert, update, and delete MiniMongo methods are disabled on the client
+// by not having allow/deny rules. This ensures more granular security & moves
+// the security logic into the meteor method. all mutation has to happen with
+// the Meteor methods. These methods are placed into the 'both' folder so that
+// Meteor uses the methods as stubs on the client, retaining the latency
+// compensation. if you need to hide the model, move the method into the
+// server directory. doing so will lose latency compensation, however a stub
+// can be created on the client folder to re-enable latency compensation.
+//
+// Methods assume you have accounts and require the caller to be logged in for
+// security. If this is not needed, remove the loggedIn check AND the ownerId
+// assignment so the ownerId is not null on create.
+//
+// Temporary `foo` and `bar` fields are created to allow automated tests to be
+// created and still pass. Soon you can pass in attr name:type at creation
 
 
 Meteor.methods({
-  // returns {String} of document id
+  /**
+   * Creates a __name-singular-pascal__ document
+   * @method
+   * @param {object} data - data to insert
+   * @returns {string} of document id
+   */
   "__name-singular-pascal__.create": function(data) {
-    console.warn("No AUTH CHECK: __name-singular-pascal__.create"); // XXX insecure method
-    //if (!this.userId) throw new Meteor.Error(401, "You must be logged in");
-    data.ownerId = this.userId;
+    var docId;
+    if (User.loggedOut()) throw new Meteor.Error(401, "Login required");
 
-    console.warn("INSECURE Model Schema: __name-singular-pascal__.create"); // XXX insecure method
-    //check(data, { key1: String, key2: Number });
+    data.ownerId = User.id();
+    data.createdAt = new Date();
 
-    var docId =  db.__name-plural-camel__.insert(data);
+    // TODO plug in your own schema
+    check(data, {
+      createdAt: Date,
+      ownerId: String,
+      // XXX temp fields
+      foo: String,
+      bar: String
+    });
+
+    docId = db.__name-plural-camel__.insert(data);
+
     console.log("  [__name-singular-pascal__.create]", docId);
     return docId;
   },
 
 
-  // Returns {Number} of documents updated
+  /**
+   * Updates a __name-singular-pascal__ document using $set
+   * @method
+   * @param {string} docId - The doc id to update
+   * @param {object} data - data to update
+   * @returns {number} of documents updated (0|1)
+   */
   "__name-singular-pascal__.update": function(docId, data) {
-    console.warn("No AUTH CHECK: __name-singular-pascal__.update"); // XXX insecure method
-    //if (!this.userId) throw new Meteor.Error(401, "You must be logged in");
+    var optional = Match.Optional;
+    var count, selector;
 
-    console.warn("INSECURE Model Schema: __name-singular-pascal__.update"); // XXX insecure method
-    //check(data, { key1: String, key2: Number });
+    check(docId, String);
+    if (User.loggedOut()) throw new Meteor.Error(401, "Login required");
+    data.updatedAt = new Date();
 
-    // if caller doesn't own document this will fail because the query field will not match
-    var count = db.__name-plural-camel__.update({_id: docId, ownerId: this.userId}, {$set: data});
-    console.log("  [__name-singular-pascal__.update]", count);
+    // TODO plug in your own schema
+    check(data, {
+      createdAt: Date,
+      updatedAt: Date,
+      ownerId: String,
+      // XXX temp fields
+      foo: optional(String),
+      bar: String
+    });
+
+    // if caller doesn't own doc, update will fail because fields won't match
+    selector = {_id: docId, ownerId: User.id()};
+
+    count = db.__name-plural-camel__.update(selector, {$set: data});
+
+    console.log("  [__name-singular-pascal__.update]", count, docId);
     return count;
   },
 
 
-  // Returns {Number} of documents deleted
+  /**
+   * Destroys a __name-singular-pascal__
+   * @method
+   * @param {string} docId - The doc id to destroy
+   * @returns {number} of documents destroyed (0|1)
+   */
   "__name-singular-pascal__.destroy": function(docId) {
-    console.warn("No AUTH CHECK: __name-singular-pascal__.destroy"); // XXX insecure method
-    //if (!this.userId) throw new Meteor.Error(401, "You must be logged in");
+    var count;
+    check(docId, String);
 
-    // if caller doesn't own document this will fail because the query field will not match
-    var count = db.__name-plural-camel__.remove({_id: docId, ownerId: this.userId});
+    if (User.loggedOut()) throw new Meteor.Error(401, "Login required");
+
+    // if caller doesn't own doc, destroy will fail because fields won't match
+    count = db.__name-plural-camel__.remove({_id: docId, ownerId: User.id()});
+
     console.log("  [__name-singular-pascal__.destroy]", count);
     return count;
   }
